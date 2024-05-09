5fc37a8a6e07ad90e0007847ec01b4511a0b85be
nathanwall <me+github@nathanwall.co.uk>
2024-05-09 16:25:45 +0100

    Completed part 2

---
+++ server/src/resolvers.ts
@@ -0,0 +1,15 @@
+import { Resolvers } from "./types";
+
+
+export const resolvers: Resolvers = {
+    Query: {
+        tracksForHome: (_, __, { dataSources }) => {
+            return dataSources.trackAPI.getTracksForHome();
+        },
+    },
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        },
+    },
+};
